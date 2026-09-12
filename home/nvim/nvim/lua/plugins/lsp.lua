c24f26816b930bb936f03a23f4ca2b2dd6e27ebc
Julian.Hareng <Julian.Hareng@gmail.com>
2026-09-12 19:21:05 +0200

    Forgot } . . .

--- home/nvim/nvim/lua/plugins/lsp.lua
+++ home/nvim/nvim/lua/plugins/lsp.lua
@@ -1,23 +1,24 @@
 return {
   -- mason
-{
-  "mason-org/mason.nvim",
-  opts = {
-    PATH = "append",
-    ensure_installed = {
-      "clang-format",
-      "prettier",
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      PATH = "append",
+      ensure_installed = {
+        "clang-format",
+        "prettier",
+      },
     },
   },
-},
   -- Configure LSP servers
-{
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      angularls = {},
-      ts_ls = {},
-      qmlls = { mason = false },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        angularls = {},
+        ts_ls = {},
+        qmlls = { mason = false },
+      },
     },
   },
-},
+}
