13cd3ca857f1fe5dd207b035ec47447f5025c5f2
Julian.Hareng <Julian.Hareng@gmail.com>
2026-09-16 20:28:42 +0200

    added hyprmod

--- home/hyprland/hypr/hyprland.lua
+++ home/hyprland/hypr/hyprland.lua
@@ -5,12 +5,12 @@ hl.config({
 	},
 })
 
--- HyprMod managed settings
-require("hyprland-gui")
-
 require("config.motions")
 require("config.keybindings")
 require("config.options")
 require("config.roules")
 require("config.animations")
 require("config.monitors")
+
+-- HyprMod managed settings
+require("~/.config/hyprmod/hyprland-gui.lua")
